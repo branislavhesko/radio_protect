3eaf677b30f048f9f1011593fcd75f941db2179b
branislavhesko <branislav.hesko@protonmail.ch>
2025-04-12 08:39:57 +0200

    eda

--- visualize.py
+++ visualize.py
@@ -3,11 +3,13 @@ from vedo import Volume, dataurl
 from vedo.applications import RayCastPlotter
 
 # Load Volume data
-volume = np.load("volume.npy")
-embryo = Volume(volume, spacing=(1, 1, 1))
-embryo.mode(1).cmap("jet")  # change visual properties
 
-# Create a Plotter instance and show
-plt = RayCastPlotter(embryo, bg='black', bg2='blackboard', axes=7)
-plt.show(viewup="z")
-plt.close()
+for idx in range(43):
+    volume = np.load(f"volume_{idx}.npy")
+    embryo = Volume(volume, spacing=(1, 1, 1))
+    embryo.mode(1).cmap("jet")  # change visual properties
+
+    # Create a Plotter instance and show
+    plt = RayCastPlotter(embryo, bg='black', bg2='blackboard', axes=7 )
+    plt.show(viewup="z")
+    plt.close()
